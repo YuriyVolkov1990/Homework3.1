d792b806d6c951f9d39acb4f243d42d2b5329a68
Professional <new99kpicem@gmail.com>
2023-08-24 22:06:30 +0300

    hogwarts.database_tests_webmvctest_+-done

---
+++ src/test/java/ru/hogwarts/school/controller/controller_mockmvc/FacultyControllerTest.java
@@ -0,0 +1,9 @@
+package ru.hogwarts.school.controller.controller_mockmvc;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import ru.hogwarts.school.controller.FacultyController;
+
+@WebMvcTest(FacultyController.class)
+public class FacultyControllerTest {
+
+}
