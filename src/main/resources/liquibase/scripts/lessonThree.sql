e5ba12634ea187101cc72ee133245b99de4a9909
Professional <new99kpicem@gmail.com>
2023-09-03 20:45:05 +0300

    hogwarts.database_hw4.3_prc

--- src/main/resources/liquibase/scripts/lessonThree.sql
+++ src/main/resources/liquibase/scripts/lessonThree.sql
@@ -5,3 +5,9 @@ create table lessonThree (
     id serial,
     name text
 )
+
+-- changeset uvolkov:2
+create index names_index on lessonThree (name);
+
+-- changeset uvolkov:2
+create index nameAndColor on lessonTree
